228d573ab020bb88e68467ea6f3427d793979b6a
Adeel Hassan <adeel.hassan2k20@gmail.com>
2026-05-11 23:19:56 +0000

    claude ok 2

--- intelligent-attack-pipeline/src/attacks/key_recovery.py
+++ intelligent-attack-pipeline/src/attacks/key_recovery.py
@@ -269,11 +269,11 @@ if NUMBA_AVAILABLE:
         
         # Extract column and compute HW
         col_input = np.uint8(0)
-        col_input |= np.uint8((state0 >> column) & 1) << 0
-        col_input |= np.uint8((state1 >> column) & 1) << 1
+        col_input |= np.uint8((state0 >> column) & 1) << 4
+        col_input |= np.uint8((state1 >> column) & 1) << 3
         col_input |= np.uint8((state2 >> column) & 1) << 2
-        col_input |= np.uint8((state3 >> column) & 1) << 3
-        col_input |= np.uint8((state4 >> column) & 1) << 4
+        col_input |= np.uint8((state3 >> column) & 1) << 1
+        col_input |= np.uint8((state4 >> column) & 1) << 0
       
         
         sbox_out = sbox[col_input]
